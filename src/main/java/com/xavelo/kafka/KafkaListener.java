dd23cc2d947eeb9c724cc8d8d0c46f717341141a
xavelo <xavelo@gmail.com>
2024-08-25 18:41:41 +0200

    save and read

--- src/main/java/com/xavelo/kafka/KafkaListener.java
+++ src/main/java/com/xavelo/kafka/KafkaListener.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,11 +21,29 @@ public class KafkaListener {
         logger.info("Received message: {}", message);
         String sql = "INSERT INTO messages (message) VALUES (?)";
         checkCollection();
-        // save message
+        saveMessage(message);
+        Message msg = findMessageByKey("key");
+        logger.info("Read message: {}", msg.getValue());
     }
 
     private void checkCollection() {
-        if (!mongoTemplate.collectionExists("test-topic")) mongoTemplate.createCollection("test-topic");
+        if (!mongoTemplate.collectionExists("test-topic")) {
+            logger.info("Creating collection test-topic");
+            mongoTemplate.createCollection("test-topic");
+        } else {
+            logger.info("Collection test-topic already existing");
+        }
+    }
+
+    private void saveMessage(String message) {
+        Message msg = new Message("key", message);
+        mongoTemplate.save(msg, "test_topic");
+        logger.info("Message with key {} saved",  msg.getKey());
+    }
+
+    private Message findMessageByKey(String key) {
+        Message msg = mongoTemplate.findOne(Query.query(Criteria.where("key").is(key)), Message.class, "test_topic");
+        return msg;
     }
 
 }
