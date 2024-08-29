44ed7f0003aae62cc61b6f6bdd046eddc974cdc2
xavelo <xavelo@gmail.com>
2024-08-29 17:15:58 +0200

    CheckDocument

--- src/main/java/com/xavelo/crypto/adapter/mongo/PriceRepository.java
+++ src/main/java/com/xavelo/crypto/adapter/mongo/PriceRepository.java
@@ -1,5 +1,14 @@
 package com.xavelo.crypto.adapter.mongo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
-public interface PriceRepository extends MongoRepository<PriceDocument, PriceDocument.CryptoPriceId> { }
+import java.time.Instant;
+import java.util.List;
+
+public interface PriceRepository extends MongoRepository<PriceDocument, PriceDocument.CryptoPriceId> {
+
+    @Query("{'_id.timestamp': {$gt: ?0}}")
+    List<PriceDocument> findByTimestampAfter(Instant timestamp);
+
+}
--- src/main/java/com/xavelo/crypto/listener/CryptoPriceUpdatesListener.java
+++ src/main/java/com/xavelo/crypto/listener/CryptoPriceUpdatesListener.java
@@ -19,6 +19,7 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.List;
 
 @Service
 public class CryptoPriceUpdatesListener {
@@ -48,25 +49,15 @@ public class CryptoPriceUpdatesListener {
         );
 
         repository.save(document);
+
+        checkDocument();
+
     }
 
-    private void checkCollection() {
-        if (!mongoTemplate.collectionExists(CRYPTO_PRICE_UPDATES_COLLECTION)) {
-            logger.info("Creating collection {}", CRYPTO_PRICE_UPDATES_COLLECTION);
-            mongoTemplate.createCollection(CRYPTO_PRICE_UPDATES_COLLECTION);
-        }
-    }
-
-    private void saveMessage(String key, String message) {
-        Message msg = new Message(key, message);
-        mongoTemplate.save(msg, CRYPTO_PRICE_UPDATES_COLLECTION);
-        logger.info("Message with key {} saved",  msg.getKey());
-    }
-
-    private Message findMessageByKey(String key) {
-        Message msg = mongoTemplate.findOne(Query.query(Criteria.where("key").is(key)), Message.class, CRYPTO_PRICE_UPDATES_COLLECTION);
-        logger.info("findMessageByKey - key {} - message {}", key, msg);
-        return msg;
+    private void checkDocument() {
+        Instant oneMinuteAgo = Instant.now().minusSeconds(60);
+        List<PriceDocument> recentDocuments = repository.findByTimestampAfter(oneMinuteAgo);
+        logger.info("Found {} documents inserted in the last minute", recentDocuments.size());
     }
 
 }
