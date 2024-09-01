9eddbd5bad31ee7308f6a22950f89bcd141a430b
xavelo <xavelo@gmail.com>
2024-09-01 18:41:24 +0200

    AveragePrice debug

--- src/main/java/com/xavelo/crypto/data/DataServiceImpl.java
+++ src/main/java/com/xavelo/crypto/data/DataServiceImpl.java
@@ -2,7 +2,11 @@ package com.xavelo.crypto.data;
 
 import com.xavelo.crypto.adapter.mongo.PriceDocument;
 import com.xavelo.crypto.adapter.mongo.PriceRepository;
+import com.xavelo.crypto.api.CryptoDataController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -13,6 +17,8 @@ import java.util.List;
 
 @Component
 public class DataServiceImpl implements DataService {
+
+    private static final Logger logger = LogManager.getLogger(DataServiceImpl.class);
 
     @Autowired
     PriceRepository priceRepository;
@@ -29,13 +35,16 @@ public class DataServiceImpl implements DataService {
 
     @Override
     public List<PriceDocument> getPricesByCoinLastHours(String coin, int hours) {
+        Sort sort = Sort.by("_id.timestamp").descending();
         Instant hoursAgo = Instant.now().minus(hours, ChronoUnit.HOURS);
-        return priceRepository.findPricesForCoinInLastHours(coin, hoursAgo);
+        return priceRepository.findPricesForCoinInLastHours(coin, hoursAgo,sort);
     }
 
     @Override
     public BigDecimal getAveragePriceByCoinLastHours(String coin, int hours) {
+        logger.info("getAveragePriceByCoinLastHours {} - {}", coin, hours);
         Date date = new Date(System.currentTimeMillis() - (long) hours * 60 * 60 * 1000);
+        logger.info("date " + date.getTime());
         List<AveragePrice> result = priceRepository.findAveragePriceInLast24Hours(coin, date);
         return result.get(0).getValue();
     }
