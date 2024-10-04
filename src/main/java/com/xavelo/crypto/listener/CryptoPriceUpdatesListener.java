9f63adaf8652443f07741b8a7d269795f7aa2aec
= <=>
2024-10-04 18:00:58 +0200

    prometheus metrics

--- src/main/java/com/xavelo/crypto/listener/CryptoPriceUpdatesListener.java
+++ src/main/java/com/xavelo/crypto/listener/CryptoPriceUpdatesListener.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.xavelo.crypto.Price;
 import com.xavelo.crypto.adapter.mongo.PriceDocument;
 import com.xavelo.crypto.adapter.mongo.PriceRepository;
+
+import java.util.concurrent.TimeUnit;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +17,9 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+
 @Service
 public class CryptoPriceUpdatesListener {
 
@@ -21,16 +27,25 @@ public class CryptoPriceUpdatesListener {
 
     private final PriceRepository repository;
     private final ObjectMapper objectMapper;
+    private final MeterRegistry meterRegistry;
     
     @Autowired
-    public CryptoPriceUpdatesListener(PriceRepository repository, ObjectMapper objectMapper) {
+    public CryptoPriceUpdatesListener(PriceRepository repository, ObjectMapper objectMapper, MeterRegistry meterRegistry) {
         this.repository = repository;
         this.objectMapper = objectMapper;
+        this.meterRegistry = meterRegistry;
     }
 
     @KafkaListener(topics = "crypto-price-updates-topic", groupId = "crypto-price-updates-group", containerFactory = "kafkaListenerContainerFactory")
     public void consume(@Payload String message, @Header(KafkaHeaders.RECEIVED_KEY) String key) throws JsonProcessingException, InterruptedException {
         logger.info("Received message: key {} - value {}", key, message);
+
+        // Start timer
+        long startTime = System.nanoTime();
+        
+        // Simulate processing time
+        Thread.sleep(5000); // Delay for 5 seconds
+
         Price price = objectMapper.readValue(message, Price.class);
         PriceDocument.PriceId priceId = new PriceDocument.PriceId(price.getCoin(), price.getTimestamp());
         PriceDocument document = new PriceDocument(
@@ -39,6 +54,19 @@ public class CryptoPriceUpdatesListener {
             price.getCurrency()
         );
         repository.save(document);
+
+        // End timer
+        long endTime = System.nanoTime();
+        
+        // Calculate processing time in milliseconds
+        long processingTime = (endTime - startTime) / 1_000_000; // Convert to milliseconds
+        
+        // Send metric to metrics server
+        Timer timer = Timer.builder("crypto.price.processing.time")
+                .description("Time taken to process crypto price updates")
+                .register(meterRegistry);
+        timer.record(processingTime, TimeUnit.MILLISECONDS);
+
     }
 
 }
