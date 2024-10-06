9b6b8d42e2b8654a3ee996928116d34f17aebcb4
= <=>
2024-10-06 23:42:46 +0200

    metrics writePriceUpdate

--- src/main/java/com/xavelo/crypto/adapter/influxdb/InfluxDBAdapter.java
+++ src/main/java/com/xavelo/crypto/adapter/influxdb/InfluxDBAdapter.java
@@ -1,28 +1,36 @@
 package com.xavelo.crypto.adapter.influxdb;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
 import com.influxdb.client.InfluxDBClient;
 import com.influxdb.client.WriteApi;
 import com.influxdb.client.domain.WritePrecision;
 import com.influxdb.client.write.Point;
-import com.xavelo.crypto.model.Price;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;    
-    
-@Service
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+
+import java.util.concurrent.TimeUnit; 
+
+import com.xavelo.crypto.model.Price; // Ensure the correct import for Price
+
+@Component
 public class InfluxDBAdapter {
 
     private static final Logger logger = LoggerFactory.getLogger(InfluxDBAdapter.class);
 
     private final InfluxDBClient influxDBClient;
+    private final MeterRegistry meterRegistry;
 
-    public InfluxDBAdapter(InfluxDBClient influxDBClient) {
+    public InfluxDBAdapter(InfluxDBClient influxDBClient, MeterRegistry meterRegistry) {
         this.influxDBClient = influxDBClient;
+        this.meterRegistry = meterRegistry;
     }
 
     public void writePriceUpdate(Price price) {
-        logger.info("writePriceUpdate");
+        long startTime = System.nanoTime();
         try (WriteApi writeApi = influxDBClient.getWriteApi()) {
             Point point = Point.measurement("crypto_price_updates")
                 .addTag("coin", price.getCoin())
@@ -32,6 +40,14 @@ public class InfluxDBAdapter {
             
             writeApi.writePoint(point);
         }
+        long endTime = System.nanoTime();
+        long processingTime = (endTime - startTime) / 1_000_000;
+        logger.info("crypto.price.save.influxdb.time: {}ms", processingTime);
+
+        Timer timer = Timer.builder("crypto.price.save.influxdb.timee")
+                .description("Time taken to save crypto price update to InfluxDB")
+                .register(meterRegistry);
+        timer.record(processingTime, TimeUnit.MILLISECONDS);
     }
 
 }
