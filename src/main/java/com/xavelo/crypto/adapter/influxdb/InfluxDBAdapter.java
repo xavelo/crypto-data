d7d9f8f68dc5eba8ebfb303f241426e0582e1534
= <=>
2024-10-07 10:56:29 +0200

    doublevalue

--- src/main/java/com/xavelo/crypto/adapter/influxdb/InfluxDBAdapter.java
+++ src/main/java/com/xavelo/crypto/adapter/influxdb/InfluxDBAdapter.java
@@ -35,7 +35,7 @@ public class InfluxDBAdapter {
             Point point = Point.measurement("crypto_price_updates")
                 .addTag("coin", price.getCoin())
                 .addTag("currency", price.getCurrency())
-                .addField("price", price.getPrice().toString())
+                .addField("price", price.getPrice().doubleValue())
                 .time(price.getTimestamp(), WritePrecision.NS);
             
             writeApi.writePoint(point);
