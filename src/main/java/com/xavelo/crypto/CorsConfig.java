801445db6673f26c9590dea35b9a69589fd0a90a
= <=>
2024-10-11 20:02:28 +0200

    CORS

---
+++ src/main/java/com/xavelo/crypto/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.xavelo.crypto;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000") // Replace with your frontend URL
+            .allowedMethods("GET")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+}
