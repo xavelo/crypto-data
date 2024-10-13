f781a2c099d21185c515a08668e0e7a2b6d0f7fe
= <=>
2024-10-13 12:57:56 +0200

    CorsFilter

--- src/main/java/com/xavelo/crypto/CryptoDataApplication.java
+++ src/main/java/com/xavelo/crypto/CryptoDataApplication.java
@@ -2,7 +2,11 @@ package com.xavelo.crypto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @PropertySource("classpath:git.properties")
@@ -11,5 +15,17 @@ public class CryptoDataApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoDataApplication.class, args);
 	}
+
+	@Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://192.168.1.94:30012");
+        config.addAllowedMethod("GET");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 
 }
