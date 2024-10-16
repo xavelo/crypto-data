a60e45791a0b16bc8a8e7135bfa661ff232b8aff
= <=>
2024-10-16 23:45:31 +0200

    error 95%

--- src/main/java/com/xavelo/crypto/listener/CryptoPriceUpdatesListener.java
+++ src/main/java/com/xavelo/crypto/listener/CryptoPriceUpdatesListener.java
@@ -67,7 +67,7 @@ public class CryptoPriceUpdatesListener {
 
                 Price price = objectMapper.readValue(record.value(), Price.class);
                 // simulate errors to test retry mechanism and observability
-                simulateUnreliableApiCall(20);
+                simulateUnreliableApiCall(95);
                 priceService.savePriceUpdate(price);
                 influxDBAdapter.writePriceUpdate(price);
 
