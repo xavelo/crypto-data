2f226bfb169c8eceed9f0b7710091b7148772773
= <=>
2024-10-16 18:12:15 +0200

    exception counter

--- src/main/java/com/xavelo/crypto/ControllerExceptionHandler.java
+++ src/main/java/com/xavelo/crypto/ControllerExceptionHandler.java
@@ -8,10 +8,21 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import io.micrometer.core.instrument.Counter; // {{ edit_1 }} Import Micrometer Counter
+import io.micrometer.core.instrument.MeterRegistry;
+
 import lombok.Data;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
+
+    private final Counter exceptionCounter;
+
+    public ControllerExceptionHandler(MeterRegistry meterRegistry) {
+        this.exceptionCounter = Counter.builder("exceptions.total") // {{ edit_6 }} Create Counter
+                .description("Total number of exceptions handled")
+                .register(meterRegistry);
+    }
 
     @Data
     public class ErrorDetails {
@@ -29,6 +40,7 @@ public class ControllerExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ErrorDetails> badCredentialsHandler(Exception ex, WebRequest request) {
+        exceptionCounter.increment();
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(),
                 request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
