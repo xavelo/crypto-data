7b12031145de3dff6192d6dd7414413a1b162eae
= <=>
2024-10-16 18:29:44 +0200

    log error

--- src/main/java/com/xavelo/crypto/ControllerExceptionHandler.java
+++ src/main/java/com/xavelo/crypto/ControllerExceptionHandler.java
@@ -2,11 +2,15 @@ package com.xavelo.crypto;
 
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+
+import com.xavelo.crypto.listener.CryptoPriceUpdatesListener;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
@@ -15,6 +19,8 @@ import lombok.Data;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(ControllerExceptionHandler.class);
 
     private final Counter exceptionCounter;
 
@@ -40,6 +46,7 @@ public class ControllerExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ErrorDetails> badCredentialsHandler(Exception ex, WebRequest request) {
+        logger.error("Exception {}", ex.getMessage(), ex);
         exceptionCounter.increment();
         ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(),
                 request.getDescription(false));
