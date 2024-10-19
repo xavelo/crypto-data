357bd61b6a6a9d8303673505fd0f93830cbf119b
= <=>
2024-10-19 21:20:27 +0200

    refactor

--- src/main/java/com/xavelo/crypto/CryptoDataApplication.java
+++ src/main/java/com/xavelo/crypto/CryptoDataApplication.java
@@ -1,5 +1,14 @@
 package com.xavelo.crypto;
 
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.Set;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -26,6 +35,32 @@ public class CryptoDataApplication {
         config.addAllowedHeader("*");
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
+    }
+
+    @Bean
+    public KafkaConsumer<String, String> dlqConsumer() {
+        String DLQ_TOPIC = "crypto-price-updates-topic-dlq";
+        System.out.println("dlq reprocess initDlqConsumer");
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "my-cluster-kafka-bootstrap.default.svc:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "dlq-reprocessor-group");
+        props.put(ConsumerConfig.CLIENT_ID_CONFIG, "dlq-reprocessor-client");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");        
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100");
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "300000"); // 5 minutes
+        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, "300000"); // 5 minutes
+        props.put(ConsumerConfig.HEARTBEAT_INTERVAL_MS_CONFIG, "3000"); // 3 seconds
+
+        KafkaConsumer<String, String> dlqConsumer = new KafkaConsumer<>(props);
+        dlqConsumer.subscribe(Collections.singletonList(DLQ_TOPIC));
+        dlqConsumer.poll(Duration.ofMillis(0)); // Ensure partitions are assigned
+        Set<TopicPartition> partitions = dlqConsumer.assignment(); // Get assigned partitions
+        System.out.println("dlq reprocess assigned partitions: {"+partitions.size()+"}");
+        dlqConsumer.pause(partitions);     
+        return dlqConsumer;    
     }
 
 }
