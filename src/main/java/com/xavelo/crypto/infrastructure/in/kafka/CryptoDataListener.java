532cac16f8a4bdca33583d16ef340813dd1b6d3b
xavelo <xavelo@gmail.com>
2025-02-07 10:30:25 +0100

    CryptoDataListener

--- src/main/java/com/xavelo/crypto/infrastructure/in/kafka/CryptoDataListener.java
+++ src/main/java/com/xavelo/crypto/infrastructure/in/kafka/CryptoDataListener.java
@@ -1,30 +1,21 @@
 package com.xavelo.crypto.infrastructure.in.kafka;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.xavelo.crypto.application.price.PriceUpdatesService;
-
-import lombok.AllArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.kafka.annotation.KafkaListener;
-
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
-public class CryptoPriceUpdatesListener {
+public class CryptoDataListener {
 
-    private static final Logger logger = LoggerFactory.getLogger(CryptoPriceUpdatesListener.class);
-
-    private PriceUpdatesService priceUpdatesService;
+    private static final Logger logger = LoggerFactory.getLogger(CryptoDataListener.class);
 
     @KafkaListener(topics = "crypto-price-updates-topic", groupId = "crypto-price-updates-group", containerFactory = "kafkaListenerContainerFactory")
     public void consume(ConsumerRecord<String, String> record, Acknowledgment acknowledgment) throws JsonProcessingException, InterruptedException {
-        logger.info("Received message: key {} - value {}", record.key(), record.value());        
-        priceUpdatesService.process(record);
+        logger.debug("Received message: key {} - value {}", record.key(), record.value());
         acknowledgment.acknowledge();
     }
 
