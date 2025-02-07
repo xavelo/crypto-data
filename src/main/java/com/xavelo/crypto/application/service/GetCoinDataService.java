fb3072324fb50eaeb072cd0920c357947363f74c
xavelo <xavelo@gmail.com>
2025-02-07 20:08:46 +0100

    GetCoinDataService

---
+++ src/main/java/com/xavelo/crypto/application/service/GetCoinDataService.java
@@ -0,0 +1,26 @@
+package com.xavelo.crypto.application.service;
+
+import com.xavelo.crypto.application.port.GetCoinDataUseCase;
+import com.xavelo.crypto.application.port.StoreCoinDataUseCase;
+import com.xavelo.crypto.domain.model.CoinData;
+import com.xavelo.crypto.domain.repository.CoinDataRepository;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class GetCoinDataService implements GetCoinDataUseCase {
+
+    private static final Logger logger = LoggerFactory.getLogger(GetCoinDataService.class);
+
+    private final CoinDataRepository coinDataRepository;
+
+    @Override
+    public CoinData getCoinData(String coin) {
+        logger.info("Getting Coin Data - {}", coin);
+        return coinDataRepository.getCoinData(coin);
+    }
+
+}
